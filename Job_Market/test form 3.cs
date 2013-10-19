df902801bb82355810f595ae01d1614dc06b7ced
festu-Kartashev <lllxxxlll5@gmail.com>
2013-10-19 11:03:05 +1100

    передвинул кнопку

---
+++ Job_Market/test form 3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Job_Market
+{
+    public partial class test_form_3 : Form
+    {
+        public test_form_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
